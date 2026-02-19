23827a0b9f5ca0a2c9b2788b80dd7e601f449591
Alexandru Molea <alexandru.molea@bemore.ro>
2026-02-19 10:24:50 +0200

    Initial Commit

---
+++ Financial Growth/Financial_GrowthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_GrowthApp.swift
+//  Financial Growth
+//
+//  Created by Alexandru Molea on 19.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Financial_GrowthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
